8556cc2b31b282d13dddafc98928b8a3165defce
chingc28 <chingchiu0728@gmail.com>
2023-05-05 02:20:51 -0700

    added code for merging file

--- Final_project_pt2.R
+++ Final_project_pt2.R
@@ -10,4 +10,23 @@ library(stringr)
 df_1 <- read.csv()
 df_2 <- read.csv()
 
+### The code goes here
+
+df_1 <- read.csv('Red.csv')
+df_2 <- read.csv('Rose.csv')
+df_3 <- read.csv('Sparkling.csv')
+df_4 <- read.csv('White.csv')
+
+types_df <- rbind(df_1, df_2, df_3, df_4)
+
+ratings_df <- read.csv('winemag-data_first150k.csv')
+
 ### The code goes here 
+
+# changing US to 'United States' to merge based on country
+
+ratings_df$country <- gsub('US', 'United States', ratings_df$country)
+
+# merging based on country, region, and winery 
+
+# df <- merge(ratings_df, types_df, by.x = 'country', by.y = 'Country')
